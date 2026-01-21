6909d4b9d4643bfac0c111d9be55c38169148445
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-01-21 08:28:14 +0200

    Server: Install typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DB_HOST'),
+        port: +configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USERNAME'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        database: configService.getOrThrow('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
