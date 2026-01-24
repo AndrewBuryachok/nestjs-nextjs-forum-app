040868e69fd4aef197e02a9cac4ca5dcf979466c
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-01-24 12:10:47 +0200

    Server: Add testing

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -2,24 +2,48 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('App', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Cards', () => {
+    it('GET /cards/my', () => {
+      return request(app.getHttpServer())
+        .get('/cards/my')
+        .expect((res) => expect(res.body.length).toBeGreaterThan(0));
+    });
+
+    it('GET /cards/all', () => {
+      return request(app.getHttpServer())
+        .get('/cards/all')
+        .expect((res) => expect(res.body.length).toBeGreaterThan(0));
+    });
+  });
+
+  describe('Transactions', () => {
+    it('GET /transactions/my', () => {
+      return request(app.getHttpServer())
+        .get('/transactions/my')
+        .expect((res) => expect(res.body.length).toBeGreaterThan(0));
+    });
+
+    it('GET /transactions/all', () => {
+      return request(app.getHttpServer())
+        .get('/transactions/all')
+        .expect((res) => expect(res.body.length).toBeGreaterThan(0));
+    });
   });
 });
