8bd1a2baf567e25881039f89c642b17c3e2be3c2
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-01-27 07:52:26 +0200

    Server: Install morgan

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
+import morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('tiny'));
   const configService = app.get(ConfigService);
   await app.listen(+configService.getOrThrow('PORT'));
 }
