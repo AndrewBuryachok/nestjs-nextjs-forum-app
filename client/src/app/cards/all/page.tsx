a98e92a765f309e1565076f67e29846819d7f0b1
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-01-27 08:06:36 +0200

    Client: Add metadata

--- client/src/app/cards/all/page.tsx
+++ client/src/app/cards/all/page.tsx
@@ -1,4 +1,9 @@
+import { generateCardsMetadata } from '@/features/cards/metadata';
 import CardsPage from '@/features/cards/components/cards-page';
+
+export function generateMetadata() {
+  return generateCardsMetadata({ tab: 'all' });
+}
 
 export default function Page() {
   return <CardsPage tab='all' />;
--- client/src/app/cards/my/page.tsx
+++ client/src/app/cards/my/page.tsx
@@ -1,4 +1,9 @@
+import { generateCardsMetadata } from '@/features/cards/metadata';
 import CardsPage from '@/features/cards/components/cards-page';
+
+export function generateMetadata() {
+  return generateCardsMetadata({ tab: 'my' });
+}
 
 export default function Page() {
   return <CardsPage tab='my' />;
