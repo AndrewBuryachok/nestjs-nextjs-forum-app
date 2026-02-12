d796708da64528481f294d0d5abb6d657ef6c993
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-02-12 13:42:50 +0200

    Client: Add edit and delete in cards

--- client/src/features/cards/actions.ts
+++ client/src/features/cards/actions.ts
@@ -1,6 +1,11 @@
 'use server';
 
-import { createCardSchema, createCardWithUserSchema } from './schema';
+import {
+  createCardSchema,
+  createCardWithUserSchema,
+  deleteCardSchema,
+  editCardSchema,
+} from './schema';
 import { actionClient } from '@/lib/safe-action';
 import { send } from '@/lib/api';
 
@@ -14,4 +19,28 @@ export const createCardWithUserAction = actionClient
   .inputSchema(createCardWithUserSchema)
   .action(({ parsedInput: body }) => {
     return send('POST', '/cards/all', body);
+  });
+
+export const editMyCardAction = actionClient
+  .inputSchema(editCardSchema)
+  .action(({ parsedInput: { cardId, ...body } }) => {
+    return send('PATCH', `/cards/${cardId}`, body);
+  });
+
+export const editUserCardAction = actionClient
+  .inputSchema(editCardSchema)
+  .action(({ parsedInput: { cardId, ...body } }) => {
+    return send('PATCH', `/cards/all/${cardId}`, body);
+  });
+
+export const deleteMyCardAction = actionClient
+  .inputSchema(deleteCardSchema)
+  .action(({ parsedInput: { cardId } }) => {
+    return send('DELETE', `/cards/${cardId}`);
+  });
+
+export const deleteUserCardAction = actionClient
+  .inputSchema(deleteCardSchema)
+  .action(({ parsedInput: { cardId } }) => {
+    return send('DELETE', `/cards/all/${cardId}`);
   });
