ac6ba44bfa065d834554a407cfee16ea4a4e9b92
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-02-21 10:50:22 +0200

    Client: Add user in auth

--- client/src/components/auth-menu.tsx
+++ client/src/components/auth-menu.tsx
@@ -3,12 +3,16 @@
 import { useTranslations } from 'next-intl';
 import { IconButton, Menu, Portal } from '@chakra-ui/react';
 import { LuLogIn, LuLogOut, LuUser } from 'react-icons/lu';
+import { useAuthContext } from '@/providers/auth-provider';
 import { useDialogContext } from '@/providers/dialog-provider';
+import CustomAvatar from './custom-avatar';
 import AuthFormWithTabs from '@/features/auth/forms/auth-form-with-tabs';
 import LogoutForm from '@/features/auth/forms/logout-form';
 
 export default function AuthMenu() {
   const t = useTranslations();
+
+  const { user } = useAuthContext();
 
   const { openDialog } = useDialogContext();
 
@@ -20,22 +24,29 @@ export default function AuthMenu() {
 
   return (
     <Menu.Root positioning={{ placement: 'bottom-end' }}>
-      <Menu.Trigger asChild cursor='pointer' focusRing='none'>
-        <IconButton size='xs' variant='ghost'>
-          <LuUser />
-        </IconButton>
+      <Menu.Trigger asChild={!user} cursor='pointer' focusRing='none'>
+        {!user ? (
+          <IconButton size='xs' variant='ghost'>
+            <LuUser />
+          </IconButton>
+        ) : (
+          <CustomAvatar user={user} />
+        )}
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content zIndex='sticky'>
-            <Menu.Item value='login' onClick={openLoginDialog}>
-              <LuLogIn />
-              {t('dialogs.login')}
-            </Menu.Item>
-            <Menu.Item value='logout' onClick={openLogoutDialog}>
-              <LuLogOut />
-              {t('dialogs.logout')}
-            </Menu.Item>
+            {!user ? (
+              <Menu.Item value='login' onClick={openLoginDialog}>
+                <LuLogIn />
+                {t('dialogs.login')}
+              </Menu.Item>
+            ) : (
+              <Menu.Item value='logout' onClick={openLogoutDialog}>
+                <LuLogOut />
+                {t('dialogs.logout')}
+              </Menu.Item>
+            )}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
--- client/src/types/tokens.ts
+++ client/src/types/tokens.ts
@@ -1,4 +1,7 @@
+import { BaseUser } from '@/features/users/types';
+
 export interface Tokens {
+  user: BaseUser;
   access: string;
   refresh: string;
 }
