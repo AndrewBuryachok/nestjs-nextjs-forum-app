c2d875adc420515044409aebbbfec5ae85b1e4c0
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-03-06 13:54:56 +0200

    Client: Add select in shops

--- client/src/features/shops/types.ts
+++ client/src/features/shops/types.ts
@@ -2,7 +2,9 @@ import { BasePlace, Place } from '../places/types';
 import { BaseUser } from '../users/types';
 import { BaseCard } from '../cards/types';
 
-export interface BaseShop extends BasePlace {
+export interface BaseShop extends BasePlace {}
+
+export interface BaseShopWithCard extends BaseShop {
   card: BaseCard;
 }
 
