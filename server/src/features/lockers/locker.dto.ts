bb7f32659ba58f49cce6f58b5f0d76694f7ba175
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-04-02 13:38:11 +0300

    Server: Add edit and delete in lockers

--- server/src/features/lockers/locker.dto.ts
+++ server/src/features/lockers/locker.dto.ts
@@ -1,5 +1,17 @@
+import { IsInt, IsNotEmpty, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 import { CreatePlaceDto, CreatePlaceWithUserDto } from '../places/place.dto';
+
+export class LockerIdDto {
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  lockerId: number;
+}
 
 export class CreateLockerDto extends CreatePlaceDto {}
 
 export class CreateLockerWithUserDto extends CreatePlaceWithUserDto {}
+
+export class EditLockerDto extends CreatePlaceDto {}
