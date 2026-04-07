0c7780ab8b3e05e27b48fd464fd8744552b751a2
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-04-07 13:28:49 +0300

    Client: Add create in orders

---
+++ client/src/features/orders/forms/create-order-form.tsx
@@ -0,0 +1,231 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useTranslations } from 'next-intl';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Field, Input, NumberInput } from '@chakra-ui/react';
+import { createMyOrderAction, createUserOrderAction } from '../actions';
+import { createOrderSchema, CreateOrderType } from '../schema';
+import { useSelectAllUsers } from '@/features/users/hooks';
+import {
+  useSelectMyCards,
+  useSelectUserCardsWithBalance,
+} from '@/features/cards/hooks';
+import { useSelectAllLockers } from '@/features/lockers/hooks';
+import { useDialogContext } from '@/providers/dialog-provider';
+import { toaster } from '@/components/ui/toaster';
+import CustomForm from '@/components/custom-form';
+import UsersCombobox from '@/features/users/components/users-combobox';
+import CardsWithBalanceCombobox from '@/features/cards/components/cards-with-balance-combobox';
+import LockersCombobox from '@/features/lockers/components/lockers-combobox';
+import ItemsCombobox from '@/components/items-combobox';
+import UnitsSegmentGroup from '@/components/units-segment-group';
+
+type Props = {
+  isAll: boolean;
+};
+
+export default function CreateOrderForm(props: Props) {
+  const t = useTranslations();
+
+  const router = useRouter();
+
+  const { closeDialog } = useDialogContext();
+
+  const form = useForm<CreateOrderType>({
+    resolver: zodResolver(createOrderSchema),
+    defaultValues: {
+      amount: 1,
+      batch: 1,
+      sum: 1,
+    },
+  });
+
+  const [userId, setUserId] = useState(0);
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    const res = props.isAll
+      ? await createUserOrderAction({ ...data, userId })
+      : await createMyOrderAction(data);
+    if (res.data) {
+      if (res.data.ok) {
+        const title = t('toasts.orders.create.success');
+        toaster.success({ title });
+        router.refresh();
+        closeDialog();
+      } else {
+        const title = res.data.message ?? t('toasts.orders.create.failure');
+        toaster.error({ title });
+      }
+    }
+  });
+
+  const users = useSelectAllUsers(props.isAll);
+  const cards = props.isAll
+    ? useSelectUserCardsWithBalance(userId)
+    : useSelectMyCards();
+  const lockers = useSelectAllLockers();
+
+  return (
+    <CustomForm disabled={form.formState.isSubmitting} onSubmit={onSubmit}>
+      {props.isAll && (
+        <Field.Root required>
+          <Field.Label>
+            {t('columns.user')}
+            <Field.RequiredIndicator />
+          </Field.Label>
+          <UsersCombobox
+            data={users.data}
+            loading={users.isLoading}
+            placeholder={t('columns.user')}
+            value={userId}
+            setValue={setUserId}
+          />
+        </Field.Root>
+      )}
+      <Field.Root disabled={props.isAll && !userId} required>
+        <Field.Label>
+          {t('columns.card')}
+          <Field.RequiredIndicator />
+        </Field.Label>
+        <Controller
+          control={form.control}
+          name='cardId'
+          render={({ field }) => (
+            <CardsWithBalanceCombobox
+              data={cards.data}
+              loading={cards.isLoading}
+              placeholder={t('columns.card')}
+              value={field.value}
+              setValue={field.onChange}
+            />
+          )}
+        />
+      </Field.Root>
+      <Field.Root disabled={props.isAll && !userId} required>
+        <Field.Label>
+          {t('columns.locker')}
+          <Field.RequiredIndicator />
+        </Field.Label>
+        <Controller
+          control={form.control}
+          name='lockerId'
+          render={({ field }) => (
+            <LockersCombobox
+              data={lockers.data}
+              loading={lockers.isLoading}
+              placeholder={t('columns.locker')}
+              value={field.value}
+              setValue={field.onChange}
+            />
+          )}
+        />
+      </Field.Root>
+      <Field.Root required>
+        <Field.Label>
+          {t('columns.item')}
+          <Field.RequiredIndicator />
+        </Field.Label>
+        <Controller
+          control={form.control}
+          name='item'
+          render={({ field }) => (
+            <ItemsCombobox value={field.value} setValue={field.onChange} />
+          )}
+        />
+      </Field.Root>
+      <Field.Root invalid={!!form.formState.errors.description}>
+        <Field.Label>{t('columns.description')}</Field.Label>
+        <Input
+          {...form.register('description')}
+          placeholder={t('columns.description')}
+        />
+        <Field.ErrorText>
+          {form.formState.errors.description?.message}
+        </Field.ErrorText>
+      </Field.Root>
+      <Field.Root invalid={!!form.formState.errors.amount} required>
+        <Field.Label>
+          {t('columns.amount')}
+          <Field.RequiredIndicator />
+        </Field.Label>
+        <Controller
+          control={form.control}
+          name='amount'
+          render={({ field }) => (
+            <NumberInput.Root
+              w='full'
+              value={String(field.value)}
+              onValueChange={(d) => field.onChange(d.valueAsNumber)}
+            >
+              <NumberInput.Control />
+              <NumberInput.Input placeholder={t('columns.amount')} />
+            </NumberInput.Root>
+          )}
+        />
+        <Field.ErrorText>
+          {form.formState.errors.amount?.message}
+        </Field.ErrorText>
+      </Field.Root>
+      <Field.Root invalid={!!form.formState.errors.batch} required>
+        <Field.Label>
+          {t('columns.batch')}
+          <Field.RequiredIndicator />
+        </Field.Label>
+        <Controller
+          control={form.control}
+          name='batch'
+          render={({ field }) => (
+            <NumberInput.Root
+              w='full'
+              value={String(field.value)}
+              onValueChange={(d) => field.onChange(d.valueAsNumber)}
+            >
+              <NumberInput.Control />
+              <NumberInput.Input placeholder={t('columns.batch')} />
+            </NumberInput.Root>
+          )}
+        />
+        <Field.ErrorText>
+          {form.formState.errors.batch?.message}
+        </Field.ErrorText>
+      </Field.Root>
+      <Field.Root required>
+        <Field.Label>
+          {t('columns.unit')}
+          <Field.RequiredIndicator />
+        </Field.Label>
+        <Controller
+          control={form.control}
+          name='unit'
+          render={({ field }) => (
+            <UnitsSegmentGroup value={field.value} setValue={field.onChange} />
+          )}
+        />
+      </Field.Root>
+      <Field.Root invalid={!!form.formState.errors.sum} required>
+        <Field.Label>
+          {t('columns.sum')}
+          <Field.RequiredIndicator />
+        </Field.Label>
+        <Controller
+          control={form.control}
+          name='sum'
+          render={({ field }) => (
+            <NumberInput.Root
+              w='full'
+              value={String(field.value)}
+              onValueChange={(d) => field.onChange(d.valueAsNumber)}
+            >
+              <NumberInput.Control />
+              <NumberInput.Input placeholder={t('columns.sum')} />
+            </NumberInput.Root>
+          )}
+        />
+        <Field.ErrorText>{form.formState.errors.sum?.message}</Field.ErrorText>
+      </Field.Root>
+    </CustomForm>
+  );
+}
