be631e0535f811fb97c345011fd7c8559083c270
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-04-21 13:40:04 +0300

    Client: Add map

---
+++ client/src/app/map/page.tsx
@@ -0,0 +1,59 @@
+import { Suspense } from 'react';
+import { getTranslations } from 'next-intl/server';
+import SvgLayout from '@/components/svg-layout';
+import CustomMap from '@/components/custom-map';
+import { Color } from '@/constants/colors';
+import { colorToVar } from '@/lib/color';
+
+export async function generateMetadata() {
+  const t = await getTranslations();
+  return { title: t('pages.map') };
+}
+
+export default function Page() {
+  const lines = [
+    { x: '50%', y: '100%', color: Color.RED },
+    { x: '50%', y: '0%', color: Color.YELLOW },
+    { x: '100%', y: '50%', color: Color.GREEN },
+    { x: '0%', y: '50%', color: Color.BLUE },
+  ];
+
+  return (
+    <SvgLayout>
+      <Suspense
+        fallback={
+          <>
+            <line
+              x1='50%'
+              y1='0%'
+              x2='50%'
+              y2='100%'
+              stroke={colorToVar('gray')}
+            />
+            <line
+              x1='0%'
+              y1='50%'
+              x2='100%'
+              y2='50%'
+              stroke={colorToVar('gray')}
+            />
+            <circle cx='50%' cy='50%' r='8' fill={colorToVar('gray')} />
+          </>
+        }
+      >
+        {lines.map((line) => (
+          <line
+            key={line.color}
+            x1='50%'
+            y1='50%'
+            x2={line.x}
+            y2={line.y}
+            stroke={colorToVar(line.color)}
+          />
+        ))}
+        <circle cx='50%' cy='50%' r='8' fill={colorToVar('purple')} />
+        <CustomMap />
+      </Suspense>
+    </SvgLayout>
+  );
+}
