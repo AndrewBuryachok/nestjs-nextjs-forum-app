eb55d4900c35fa11dd9be827dbd317c3518d4ee6
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-05-13 20:38:06 +0300

    Server: Add update user role

--- server/src/features/users/user-errors.enum.ts
+++ server/src/features/users/user-errors.enum.ts
@@ -1,7 +1,11 @@
 export enum UserError {
   NICK_ALREADY_USED = 'users.nick_already_used',
+  ALREADY_HAVE_ROLE = 'users.already_have_role',
+  NOT_HAVE_ROLE = 'users.not_have_role',
   NOT_FOUND = 'users.not_found',
   CREATE_FAILED = 'users.create_failed',
   SET_TOKEN_FAILED = 'users.set_token_failed',
   RESET_TOKEN_FAILED = 'users.reset_token_failed',
+  ADD_ROLE_FAILED = 'users.add_role_failed',
+  REMOVE_ROLE_FAILED = 'users.remove_role_failed',
 }
