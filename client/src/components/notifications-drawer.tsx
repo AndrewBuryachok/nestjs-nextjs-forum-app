54ceed5071de5acbe4858c1e5c53770154dfce60
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-06-19 20:04:17 +0300

    Client: Add notifications drawer timeline

--- client/src/components/notifications-drawer.tsx
+++ client/src/components/notifications-drawer.tsx
@@ -1,12 +1,23 @@
+import { useTranslations } from 'next-intl';
 import { IconButton } from '@chakra-ui/react';
 import { LuBell } from 'react-icons/lu';
+import CustomDrawer from './custom-drawer';
+import NotificationsTimeline from './notifications-timeline';
 import NotificationsCircle from './notifications-circle';
 
 export default function NotificationsDrawer() {
+  const t = useTranslations();
+
   return (
-    <IconButton size='xs' variant='ghost'>
-      <LuBell />
-      <NotificationsCircle />
-    </IconButton>
+    <CustomDrawer
+      placement='end'
+      title={t('drawers.notifications.title')}
+      body={<NotificationsTimeline />}
+    >
+      <IconButton size='xs' variant='ghost'>
+        <LuBell />
+        <NotificationsCircle />
+      </IconButton>
+    </CustomDrawer>
   );
 }
