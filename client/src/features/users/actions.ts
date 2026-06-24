2bb772145184c5c0b94ffbe185ce8d692231b883
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-06-24 20:40:16 +0300

    Client: Add edit my profile

--- client/src/features/users/actions.ts
+++ client/src/features/users/actions.ts
@@ -2,11 +2,26 @@
 
 import {
   changeUserPasswordSchema,
+  editMyProfileSchema,
   editUserProfileSchema,
   updateUserRoleSchema,
 } from './schema';
 import { actionClient } from '@/lib/safe-action';
 import { send } from '@/lib/api';
+import { createSession, verifySession } from '@/lib/session';
+
+export const editMyProfileAction = actionClient
+  .inputSchema(editMyProfileSchema)
+  .action(async ({ parsedInput: body }) => {
+    const res = await send('PATCH', '/users/me/profile', body);
+    if (res.ok) {
+      const session = await verifySession();
+      if (session) {
+        await createSession({ ...session.user, avatar: body.avatar });
+      }
+    }
+    return res;
+  });
 
 export const editUserProfileAction = actionClient
   .inputSchema(editUserProfileSchema)
