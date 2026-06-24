39f84a956df5e16d9b0010433e271aba51b7c544
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-06-24 13:48:33 +0300

    Client: Add edit user profile

--- client/src/features/users/components/users-actions.tsx
+++ client/src/features/users/components/users-actions.tsx
@@ -1,6 +1,7 @@
 import { PAGE_TABS_MAP } from '@/config/navigation';
 import { User } from '../types';
 import { viewUserAction } from '../actions/view-user-action';
+import { editUserProfileAction } from '../actions/edit-user-profile-action';
 import { addUserRoleAction } from '../actions/add-user-role-action';
 import { removeUserRoleAction } from '../actions/remove-user-role-action';
 import CustomActions from '@/components/custom-actions';
@@ -13,7 +14,7 @@ type Props = {
 export default function UsersActions(props: Props) {
   const actions = {
     main: [],
-    all: [addUserRoleAction, removeUserRoleAction],
+    all: [editUserProfileAction, addUserRoleAction, removeUserRoleAction],
   }[props.tab];
 
   return (
--- client/src/features/users/schema.ts
+++ client/src/features/users/schema.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod';
 import { Role } from '@/constants/roles';
 
+export const editUserProfileSchema = z.object({
+  userId: z.number().int().min(1),
+  avatar: z.string().min(3).max(16).or(z.literal('')),
+});
+
+export type EditUserProfileType = z.infer<typeof editUserProfileSchema>;
+
 export const updateUserRoleSchema = z.object({
   userId: z.number().int().min(1),
   role: z.enum(Role),
