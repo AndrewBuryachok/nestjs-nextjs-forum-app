069c104ce0e78530ae8bbcc4c23542fc446db4b9
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-06-30 13:22:05 +0300

    Client: Add form in search

--- client/src/components/search-button.tsx
+++ client/src/components/search-button.tsx
@@ -1,9 +1,21 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
 import { IconButton } from '@chakra-ui/react';
 import { LuSearch } from 'react-icons/lu';
+import { useDialogContext } from '@/providers/dialog-provider';
+import SearchForm from './search-form';
 
 export default function SearchButton() {
+  const t = useTranslations();
+
+  const { openDialog } = useDialogContext();
+
+  const onClick = () =>
+    openDialog({ title: t('dialogs.search'), body: <SearchForm /> });
+
   return (
-    <IconButton size='xs' variant='ghost'>
+    <IconButton size='xs' variant='ghost' onClick={onClick}>
       <LuSearch />
     </IconButton>
   );
