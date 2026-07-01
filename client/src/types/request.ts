84389776b453d724bb53a8f2e332651f353a1f03
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-07-01 07:22:07 +0300

    Client: Add user in search

--- client/src/types/request.ts
+++ client/src/types/request.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const requestSchema = z.object({
   page: z.coerce.number().int().min(1).catch(1),
   id: z.coerce.number().int().min(1).optional().catch(undefined),
+  user: z.coerce.number().int().min(1).optional().catch(undefined),
 });
 
 export type Request = z.infer<typeof requestSchema>;
--- client/src/types/search.ts
+++ client/src/types/search.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const searchSchema = z.object({
   id: z.number().int().min(1).optional(),
+  user: z.number().int().min(0).optional(),
 });
 
 export type SearchType = z.infer<typeof searchSchema>;
