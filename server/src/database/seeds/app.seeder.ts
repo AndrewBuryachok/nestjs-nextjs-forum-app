58c0e73427c80ea7e7defc4c4af0b57f29b87a0d
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-07-30 13:36:09 +0300

    Server: Remove logging from seeder

--- server/src/database/seeds/app.seeder.ts
+++ server/src/database/seeds/app.seeder.ts
@@ -1,4 +1,3 @@
-import { Logger } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { faker } from '@faker-js/faker';
@@ -15,15 +14,10 @@ import { Order } from '../../features/orders/order.entity';
 import { Status, TransactionType } from '../../common/enums';
 
 export default class AppSeeder implements Seeder {
-  private logger = new Logger(AppSeeder.name);
-
   public async run(
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
   ) {
-    const startTime = Date.now();
-    this.logger.log('🚀 Starting Seeding Process...');
-    this.logger.log('Generating Users...');
     const userFactory = factoryManager.get(User);
     const users: User[] = [];
     for (let i = 0; i < 20; i++) {
@@ -31,8 +25,6 @@ export default class AppSeeder implements Seeder {
       const user = await userFactory.make({ id });
       users.push(user);
     }
-    this.logger.log(`Generated ${users.length} Users`);
-    this.logger.log('Generating Cards...');
     const cardFactory = factoryManager.get(Card);
     const cards: Card[] = [];
     for (let i = 0; i < 40; i++) {
@@ -41,8 +33,6 @@ export default class AppSeeder implements Seeder {
       const card = await cardFactory.make({ id, user });
       cards.push(card);
     }
-    this.logger.log(`Generated ${cards.length} Cards`);
-    this.logger.log('Generating CardsUsers...');
     const cardUserFactory = factoryManager.get(CardUser);
     const cardsUsers: CardUser[] = [];
     for (const card of cards) {
@@ -55,8 +45,6 @@ export default class AppSeeder implements Seeder {
         cardsUsers.push(cardUser);
       }
     }
-    this.logger.log(`Generated ${cardsUsers.length} CardsUsers`);
-    this.logger.log('Generating Transactions...');
     const randomUserOf = (card: Card) =>
       faker.helpers.arrayElement(
         cardsUsers.filter((cardUser) => cardUser.card.id === card.id),
@@ -96,8 +84,6 @@ export default class AppSeeder implements Seeder {
         transactions.push(transaction);
       }
     }
-    this.logger.log(`Generated ${transactions.length} Transactions`);
-    this.logger.log('Generating Transfers...');
     const transfers: Transaction[] = [];
     for (let i = 0; i < 40; i++) {
       const sum = faker.number.int({ min: 1, max: 1000 });
@@ -121,8 +107,6 @@ export default class AppSeeder implements Seeder {
       transfers.push(transfer);
     }
     transactions.push(...transfers);
-    this.logger.log(`Generated ${transfers.length} Transfers`);
-    this.logger.log('Generating Invoices...');
     const invoiceFactory = factoryManager.get(Invoice);
     const invoices: Invoice[] = [];
     for (let i = 0; i < 40; i++) {
@@ -157,8 +141,6 @@ export default class AppSeeder implements Seeder {
       }
       invoices.push(invoice);
     }
-    this.logger.log(`Generated ${invoices.length} Invoices`);
-    this.logger.log('Generating Shops...');
     const shopFactory = factoryManager.get(Shop);
     const shops: Shop[] = [];
     for (let i = 0; i < 20; i++) {
@@ -168,8 +150,6 @@ export default class AppSeeder implements Seeder {
       const shop = await shopFactory.make({ id, user, card });
       shops.push(shop);
     }
-    this.logger.log(`Generated ${shops.length} Shops`);
-    this.logger.log('Generating Products...');
     const productFactory = factoryManager.get(Product);
     const products: Product[] = [];
     for (let i = 0; i < 40; i++) {
@@ -179,8 +159,6 @@ export default class AppSeeder implements Seeder {
       const product = await productFactory.make({ id, shop, user });
       products.push(product);
     }
-    this.logger.log(`Generated ${products.length} Products`);
-    this.logger.log('Generating Purchases...');
     const purchaseFactory = factoryManager.get(Purchase);
     const purchases: Purchase[] = [];
     for (let i = 0; i < 40; i++) {
@@ -219,8 +197,6 @@ export default class AppSeeder implements Seeder {
       });
       purchases.push(purchase);
     }
-    this.logger.log(`Generated ${purchases.length} Purchases`);
-    this.logger.log('Generating Lockers...');
     const lockerFactory = factoryManager.get(Locker);
     const lockers: Locker[] = [];
     for (let i = 0; i < 20; i++) {
@@ -229,8 +205,6 @@ export default class AppSeeder implements Seeder {
       const locker = await lockerFactory.make({ id, user });
       lockers.push(locker);
     }
-    this.logger.log(`Generated ${lockers.length} Lockers`);
-    this.logger.log('Generating Orders...');
     const orderFactory = factoryManager.get(Order);
     const orders: Order[] = [];
     for (let i = 0; i < 40; i++) {
@@ -288,41 +262,15 @@ export default class AppSeeder implements Seeder {
       }
       orders.push(order);
     }
-    this.logger.log(`Generated ${orders.length} Orders`);
-    this.logger.log('💾 Saving generated entities to DB...');
-    this.logger.log('Saving Users...');
     await dataSource.getRepository(User).save(users);
-    this.logger.log(`Saved ${users.length} Users`);
-    this.logger.log('Saving Cards...');
     await dataSource.getRepository(Card).save(cards);
-    this.logger.log(`Saved ${cards.length} Cards`);
-    this.logger.log('Saving CardsUsers...');
     await dataSource.getRepository(CardUser).save(cardsUsers);
-    this.logger.log(`Saved ${cardsUsers.length} CardsUsers`);
-    this.logger.log('Saving Transactions...');
     await dataSource.getRepository(Transaction).save(transactions);
-    this.logger.log(`Saved ${transactions.length} Transactions`);
-    this.logger.log('Saving Invoices...');
     await dataSource.getRepository(Invoice).save(invoices);
-    this.logger.log(`Saved ${invoices.length} Invoices`);
-    this.logger.log('Saving Shops...');
     await dataSource.getRepository(Shop).save(shops);
-    this.logger.log(`Saved ${shops.length} Shops`);
-    this.logger.log('Saving Products...');
     await dataSource.getRepository(Product).save(products);
-    this.logger.log(`Saved ${products.length} Products`);
-    this.logger.log('Saving Purchases...');
     await dataSource.getRepository(Purchase).save(purchases);
-    this.logger.log(`Saved ${purchases.length} Purchases`);
-    this.logger.log('Saving Lockers...');
     await dataSource.getRepository(Locker).save(lockers);
-    this.logger.log(`Saved ${lockers.length} Lockers`);
-    this.logger.log('Saving Orders...');
     await dataSource.getRepository(Order).save(orders);
-    this.logger.log(`Saved ${orders.length} Orders`);
-    const duration = ((Date.now() - startTime) / 1000).toFixed(2);
-    this.logger.log(
-      `✅ Seeding process completed successfully in ${duration}s`,
-    );
   }
 }
