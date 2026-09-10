5b95fc56b133c9f567b4df2bdae77bf57329e1e8
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-09-10 08:30:05 +0300

    Server: Rename invoices to fines

---
+++ server/src/features/fines/fines.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Fine } from './fine.entity';
+import { CardsModule } from '../cards/cards.module';
+import { TransactionsModule } from '../transactions/transactions.module';
+import { FinesController } from './fines.controller';
+import { FinesService } from './fines.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Fine]), CardsModule, TransactionsModule],
+  controllers: [FinesController],
+  providers: [FinesService],
+})
+export class FinesModule {}
