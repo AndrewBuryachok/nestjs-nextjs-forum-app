1f96033126a77117342960eab2e020527d2baaa4
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-09-10 20:02:24 +0300

    Client: Add check has user in cards

--- client/src/features/cards/actions/delete-card-action.tsx
+++ client/src/features/cards/actions/delete-card-action.tsx
@@ -7,6 +7,7 @@ export const deleteCardFactory = (card: Card, isAll: boolean) => ({
   action: 'delete',
   dialog: 'card',
   color: Color.RED,
+  disabled: card.users > 1,
   userId: isAll ? 0 : card.user.id,
   icon: <LuTrash2 />,
   body: <DeleteCardForm card={card} isAll={isAll} />,
