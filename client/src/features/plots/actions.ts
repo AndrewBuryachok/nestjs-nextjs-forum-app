d1cebba6a644318c79167706e6a048b734d44e6c
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-09-12 11:18:48 +0300

    Client: Add edit and delete in plots

--- client/src/features/plots/actions.ts
+++ client/src/features/plots/actions.ts
@@ -1,6 +1,11 @@
 'use server';
 
-import { createPlotSchema, createPlotWithUserSchema } from './schema';
+import {
+  createPlotSchema,
+  createPlotWithUserSchema,
+  deletePlotSchema,
+  editPlotSchema,
+} from './schema';
 import { actionClient } from '@/lib/safe-action';
 import { send } from '@/lib/api';
 
@@ -14,4 +19,28 @@ export const createUserPlotAction = actionClient
   .inputSchema(createPlotWithUserSchema)
   .action(({ parsedInput: body }) => {
     return send('POST', '/plots/all', body);
+  });
+
+export const editMyPlotAction = actionClient
+  .inputSchema(editPlotSchema)
+  .action(({ parsedInput: { plotId, ...body } }) => {
+    return send('PATCH', `/plots/${plotId}`, body);
+  });
+
+export const editUserPlotAction = actionClient
+  .inputSchema(editPlotSchema)
+  .action(({ parsedInput: { plotId, ...body } }) => {
+    return send('PATCH', `/plots/all/${plotId}`, body);
+  });
+
+export const deleteMyPlotAction = actionClient
+  .inputSchema(deletePlotSchema)
+  .action(({ parsedInput: { plotId } }) => {
+    return send('DELETE', `/plots/${plotId}`);
+  });
+
+export const deleteUserPlotAction = actionClient
+  .inputSchema(deletePlotSchema)
+  .action(({ parsedInput: { plotId } }) => {
+    return send('DELETE', `/plots/all/${plotId}`);
   });
